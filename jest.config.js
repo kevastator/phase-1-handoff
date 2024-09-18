6f17401c17a4695f813569e56ac30066adab4a98
MadSons <madsenc004@gmail.com>
2024-09-18 18:16:19 -0400

    update 2

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/Phase 1/src/tests/**/*.test.ts'],
-  };
-  
